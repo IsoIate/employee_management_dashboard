7b52ec9c56db7db00f61ba5ae1d63923296ae27b
Isolate <50405art@naver.com>
2025-09-01 05:15:36 +0900

    삭제후 재설치

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,9 +1,7 @@
 'use client'
 
 import { SessionProvider } from 'next-auth/react';
-import { setupChartJS } from "@/lib/chart-setup";
 
 export function Providers({ children }) {
-    setupChartJS();
     return <SessionProvider>{children}</SessionProvider>;
 }
