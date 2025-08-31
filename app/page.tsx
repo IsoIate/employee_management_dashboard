8e92c89f8ca03940ad2bc0cbd9f554b42ddc4bba
Isolate <50405art@naver.com>
2025-09-01 05:28:07 +0900

    통합 테스트

--- app/page.tsx
+++ app/page.tsx
@@ -40,12 +40,12 @@ export default function Dashboard() {
         <h1 className="text-2xl font-bold mb-4">대시보드</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="grid grid-cols-1 gap-4">
-            {/* <TotalEmployeesChartCard employees={employees} /> */}
-            {/* <AgeChartCard employees={employees} className="w-full " /> */}
+            <TotalEmployeesChartCard employees={employees} />
+            <AgeChartCard employees={employees} className="w-full " />
           </div>
           <div className="grid grid-cols-1 gap-4 w-full h-full">
-            {/* <GenderChartCard employees={employees} /> */}
-            {/* <DepartmentChartCard employees={employees} className="w-full " /> */}
+            <GenderChartCard employees={employees} />
+            <DepartmentChartCard employees={employees} className="w-full " />
           </div>
           <div className="col-span-1 md:col-span-2">
             <PersonnelChartCard
